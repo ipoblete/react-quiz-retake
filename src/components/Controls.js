21d9c6be81c5f6d77c701c44a6a1935b6d28bc9c
Carmen V Ramos <carmenvramos@yahoo.com>
2019-03-01 11:49:23 -0800

    Added App test

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Controls({ title, subtitle }) {
+function Controls({ title, subtitle, onChange }) {
 
   return (
     <form>
