6ba0f6e2c5af460bd0594c085f3bd534863d95c9
Ivan <i.poblete915@gmail.com>
2019-03-06 15:54:51 -0800

    wired up App

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  state = {
+    title: 'React',
+    subtitle: 'Make-up Quiz'
+  };
+
+  render() {
+    return (
+      <>
+        <h1>REACT MAKE-UP!</h1>
+      </>
+    );
+  }
+}
